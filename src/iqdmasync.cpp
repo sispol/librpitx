c9f81d87335bb7e47a749606079ad5390c05990d
Oscar Pereyra <Oscarlper@gmail.com>
2022-12-01 11:51:01 -0300

    Update iqdmasync.cpp
    GPIO 4 CLK by default changed to GPIO 20

--- src/iqdmasync.cpp
+++ src/iqdmasync.cpp
@@ -65,7 +65,7 @@ iqdmasync::iqdmasync(uint64_t TuneFrequency,uint32_t SR,int Channel,uint32_t Fif
 iqdmasync::~iqdmasync()
 {
 	clkgpio::gengpio.gpioreg[GPFSEL0]=Originfsel;
-	clkgpio::disableclk(4);	
+	clkgpio::disableclk(20); // GPIO 4 CLK by default changed to GPIO 20	 
 }
 
 void iqdmasync::SetPhase(bool inversed)
